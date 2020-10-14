7453d88678e07c82fc63d11b55ec38dc41baa81b
virginiacc <vczosek@gmail.com>
2020-10-14 08:28:01 -0700

    Add new page images and update preview time.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 
 function init() {
-    var button = document.getElementById('start');
-    var overlay = document.getElementById('overlay');
-    var message = document.getElementById('success-message');
-    button.addEventListener('click', function () {
+    var button = document.getElementById( 'start' );
+    var overlay = document.getElementById( 'overlay' );
+    var message = document.getElementById( 'success-message' );
+    button.addEventListener( 'click', function() {
         overlay.style.display = 'none';
         button.style.display = 'none';
-        var hide = setTimeout(function() {
+        var hide = setTimeout( function() {
             overlay.style.display = 'block';
             message.style.display = 'block';
-        }, 5000);
+        }, 10000 );
     });
 }
 
